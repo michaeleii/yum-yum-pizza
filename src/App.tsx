eec1d2081d429a5f9a42f4dd8071f8a825ffdb1e
michaellei-e0ca <michaelleie0ca@gmail.com>
2023-06-24 18:45:08 -0700

    Add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
-import { createBrowserRouter } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
 
-createBrowserRouter([
+const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
   },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  {
+    path: "/order/new",
+    element: <CreateOrder />,
+  },
+  {
+    path: "/order/:orderId",
+    element: <Order />,
+  },
 ]);
 
 function App() {
-  return <div>App</div>;
+  return <RouterProvider router={router} />;
 }
 export default App;
