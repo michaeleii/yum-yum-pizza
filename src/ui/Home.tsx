ffd41248983e4f96f0c7ee1a8dea52e23b534473
michaellei-e0ca <michaelleie0ca@gmail.com>
2023-06-26 16:38:13 -0700

    fix hero image styling

--- src/ui/Home.tsx
+++ src/ui/Home.tsx
@@ -7,7 +7,7 @@ function Home() {
   const username = useAppSelector(getUsername);
   return (
     <div className="my-10 flex flex-col justify-center gap-10 px-4 text-center sm:my-16 lg:flex-row">
-      <div className="md:mt-5 md:text-left">
+      <div className="md:mt-5 lg:text-left">
         <h1 className="mb-8 text-xl font-semibold md:text-3xl">
           The best pizza.
           <br />
@@ -18,7 +18,7 @@ function Home() {
         {!username ? (
           <CreateUser />
         ) : (
-          <div className="w-72 text-center">
+          <div className="mx-auto w-72 text-center">
             <Button to="menu" type="small">
               Continue ordering, {username}
             </Button>
@@ -28,7 +28,7 @@ function Home() {
       <img
         src="https://images.unsplash.com/photo-1588315029754-2dd089d39a1a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1171&q=80"
         alt=""
-        className=" rounded-lg shadow-xl md:h-96"
+        className=" rounded-lg shadow-xl lg:h-96"
       />
     </div>
   );
