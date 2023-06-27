3504eb36e0f75942192414be6cdb863089b5be85
michaellei-e0ca <michaelleie0ca@gmail.com>
2023-06-26 17:14:39 -0700

    Restyle the delete btn

--- src/features/cart/DeleteItem.tsx
+++ src/features/cart/DeleteItem.tsx
@@ -1,3 +1,4 @@
+import { TrashIcon } from "@heroicons/react/24/outline";
 import { useAppDispatch } from "../../hook";
 import Button from "../../ui/Button";
 import { removeFromCart } from "./cartSlice";
@@ -5,8 +6,8 @@ import { removeFromCart } from "./cartSlice";
 function DeleteItem({ id }: { id: number }) {
   const dispatch = useAppDispatch();
   return (
-    <Button type="small" onClick={() => dispatch(removeFromCart(id))}>
-      Delete
+    <Button type="danger" onClick={() => dispatch(removeFromCart(id))}>
+      <TrashIcon className="h-5 w-5" />
     </Button>
   );
 }
