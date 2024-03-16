71b19484cc2235acf3e447701015ed515dd6b4fa
Michael Lei <michaelleie0ca@gmail.com>
2024-03-16 14:39:52 -0700

    Finish homepage

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,20 +1,34 @@
 import { Link } from "@tanstack/react-router";
-import { Button } from "./ui/button";
 
 export default function Navbar() {
   return (
-    <header className="bg-primary">
+    <header className="bg-primary shadow-lg">
       <div className="mx-auto flex max-w-7xl items-center justify-between p-5">
         <Link to="/" className="[&.active]:font-bold">
-          <div className="text-3xl font-bold">🍕Yum Yum Pizza</div>
+          <div className="flex items-baseline gap-2 text-2xl font-bold">
+            <span>🍕</span>
+            <span>Yum Yum Pizza</span>
+          </div>
         </Link>
-        <nav className="flex items-center gap-6">
-          <Link to="/menu" className="[&.active]:font-bold">
+        <nav className="flex items-center gap-6 font-medium">
+          <Link
+            to="/"
+            className="h-10 rounded-lg px-4 py-2 transition-all hover:bg-stone-700  hover:text-white [&.active]:bg-stone-700 [&.active]:text-white"
+          >
+            Home
+          </Link>
+          <Link
+            to="/menu"
+            className="h-10 rounded-lg px-4 py-2 transition-all hover:bg-stone-700  hover:text-white [&.active]:bg-stone-700 [&.active]:text-white"
+          >
             Menu
           </Link>
-          <Button className="text-md bg-stone-700 text-white transition-all hover:bg-stone-600">
+          <Link
+            to="/order"
+            className="h-10 rounded-lg px-4 py-2 transition-all hover:bg-stone-700  hover:text-white [&.active]:bg-stone-700 [&.active]:text-white"
+          >
             Order
-          </Button>
+          </Link>
         </nav>
       </div>
     </header>
--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -10,62 +10,9 @@ function Home() {
     <main className="pb-6">
       <HeroSection />
       <AboutSection />
-      <section className="mx-auto max-w-7xl px-6 py-20">
-        <h2 className="text-4xl font-bold md:text-5xl">
-          What Our Customers Say
-        </h2>
-      </section>
+      <TestominalsSection />
       <CTASection />
     </main>
-  );
-}
-
-function CTASection() {
-  return (
-    <section className="mx-auto flex max-w-7xl flex-col items-center justify-between gap-6 rounded-lg border bg-stone-700 px-6 py-20 text-white md:flex-row">
-      <h2 className="text-4xl font-bold md:text-5xl">Ready to Order?</h2>
-      <div className="space-x-6">
-        <Button
-          size="lg"
-          className="rounded-none text-xl shadow-md hover:bg-yellow-500"
-        >
-          Order now
-        </Button>
-        <Button
-          size="lg"
-          variant="outline"
-          className="rounded-none bg-transparent text-xl shadow-md"
-        >
-          Explore Menu
-        </Button>
-      </div>
-    </section>
-  );
-}
-
-function AboutSection() {
-  return (
-    <section className="mx-auto max-w-7xl px-6 py-20">
-      <div className="flex flex-col items-center gap-6 md:flex-row md:justify-between">
-        <div className="space-y-4 sm:max-w-xl">
-          <p className="font-semibold">Simply Delicious</p>
-          <h2 className="text-4xl font-bold md:text-5xl">
-            Serving Pizza with Passion Since 1985
-          </h2>
-          <p>
-            Our pizza restaurant was founded in 1985 by John & Jane Doe who
-            wanted to bring the authentic taste of Italy to our local community.
-            Over the years, we have grown and evolved, but our commitment to
-            quality and customer satisfaction remains unchanged.
-          </p>
-        </div>
-        <img
-          src="https://images.unsplash.com/photo-1604382354936-07c5d9983bd3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          alt="Pizza"
-          className="max-w-xl rounded-md object-cover"
-        />
-      </div>
-    </section>
   );
 }
 
@@ -99,6 +46,139 @@ function HeroSection() {
           </Button>
         </div>
       </div>
+    </section>
+  );
+}
+
+function AboutSection() {
+  return (
+    <section className="mx-auto max-w-7xl px-6 py-20">
+      <div className="flex flex-col items-center gap-6 md:flex-row md:justify-between">
+        <div className="space-y-4 sm:max-w-xl">
+          <p className="font-semibold text-yellow-600">Simply Delicious</p>
+          <h2 className="text-4xl font-bold md:text-5xl">
+            Serving Pizza with Passion Since 1985
+          </h2>
+          <p>
+            Our pizza restaurant was founded in 1985 by John & Jane Doe who
+            wanted to bring the authentic taste of Italy to our local community.
+            Over the years, we have grown and evolved, but our commitment to
+            quality and customer satisfaction remains unchanged.
+          </p>
+        </div>
+        <img
+          src="https://images.unsplash.com/photo-1604382354936-07c5d9983bd3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="Pizza"
+          className="max-w-xl rounded-md object-cover"
+        />
+      </div>
+    </section>
+  );
+}
+
+function CTASection() {
+  return (
+    <section className="mx-auto flex max-w-7xl flex-col items-center justify-between gap-6 rounded-lg border bg-stone-700 px-6 py-20 text-white md:flex-row">
+      <h2 className="text-4xl font-bold md:text-5xl">Ready to Order?</h2>
+      <div className="space-x-6">
+        <Button
+          size="lg"
+          className="rounded-none text-xl shadow-md hover:bg-yellow-500"
+        >
+          Order now
+        </Button>
+        <Button
+          size="lg"
+          variant="outline"
+          className="rounded-none bg-transparent text-xl shadow-md"
+        >
+          Explore Menu
+        </Button>
+      </div>
+    </section>
+  );
+}
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import Autoplay from "embla-carousel-autoplay";
+import { StarHalfIcon, StarIcon } from "lucide-react";
+function TestominalsSection() {
+  const testimonials = [
+    {
+      avatar: "https://randomuser.me/api/portraits/men/79.jpg",
+      name: "John Doe",
+      comment:
+        "I've been eating at Yum Yum Pizza for over 10 years and I've never been disappointed. The pizza is always fresh and tasty.",
+      rating: 5,
+    },
+    {
+      avatar: "https://randomuser.me/api/portraits/women/27.jpg",
+      name: "Jane Doe",
+      comment:
+        "I love the variety of pizzas available at Yum Yum Pizza. I can always find something new to try.",
+      rating: 4.5,
+    },
+    {
+      avatar: "https://randomuser.me/api/portraits/lego/1.jpg",
+      name: "John Smith",
+      comment:
+        "The staff at Yum Yum Pizza are always friendly and helpful. I love the atmosphere of the restaurant.",
+      rating: 5,
+    },
+  ];
+  return (
+    <section className="mx-auto max-w-7xl px-6 py-20">
+      <h2 className="mb-10 text-4xl font-bold md:text-5xl">
+        What our customers say
+      </h2>
+      <Carousel
+        plugins={[
+          Autoplay({
+            delay: 5000,
+          }),
+        ]}
+      >
+        <CarouselContent>
+          {testimonials.map((testimonial, index) => (
+            <CarouselItem key={index}>
+              <div className="flex flex-col gap-4 rounded-lg bg-gray-50 p-6">
+                <div className="flex items-center gap-4">
+                  {Array.from({
+                    length: Math.floor(testimonial.rating) - 1,
+                  }).map((_, index) => (
+                    <StarIcon
+                      key={index}
+                      className="h-6 w-6 fill-yellow-500 stroke-yellow-500"
+                    />
+                  ))}
+                  {testimonial.rating % 1 !== 0 && (
+                    <StarHalfIcon className="h-6 w-6 fill-yellow-500 stroke-yellow-500" />
+                  )}
+                </div>
+                <div className="py-4 text-2xl font-semibold">
+                  "{testimonial.comment}"
+                </div>
+                <div className="flex items-center gap-6">
+                  <img
+                    src={testimonial.avatar}
+                    alt={testimonial.name}
+                    className="h-16 w-16 rounded-full"
+                  />
+                  <div className="font-semibold">{testimonial.name}</div>
+                </div>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious className="border-none" />
+        <CarouselNext className="border-none" />
+      </Carousel>
     </section>
   );
 }
