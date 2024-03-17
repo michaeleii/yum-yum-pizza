3308809dcabad6621356d5a0b8875144828e5105
Michael Lei <michaelleie0ca@gmail.com>
2024-03-17 10:34:00 -0700

    Add footer component

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -11,11 +11,17 @@ export const Route = createRootRoute({
       <Suspense>
         <TanStackRouterDevtools />
       </Suspense>
-      <footer>
-        <div className="mt-20 bg-primary py-10 text-center text-xl">
-          &copy; {new Date().getFullYear()} Yum Yum Pizza
-        </div>
-      </footer>
+      <Footer />
     </>
   ),
 });
+
+function Footer() {
+  return (
+    <footer>
+      <div className="mt-20 bg-primary py-10 text-center text-xl">
+        &copy; {new Date().getFullYear()} Yum Yum Pizza
+      </div>
+    </footer>
+  );
+}
