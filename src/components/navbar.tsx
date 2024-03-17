db7db35471e3a7b5d23f5575d5919df453bbcca6
Michael Lei <michaelleie0ca@gmail.com>
2024-03-16 20:36:22 -0700

    Add cart sheet

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,20 +1,14 @@
 import { Link } from "@tanstack/react-router";
 
-import {
-  Drawer,
-  DrawerContent,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerTrigger,
-} from "@/components/ui/drawer";
 import { Button } from "./ui/button";
 import { CartItem, useCartStore } from "@/store/cart-store";
+import { useMediaQuery } from "usehooks-ts";
 
 export default function Navbar() {
+  const isDesktop = useMediaQuery("(min-width: 768px)");
   const items = useCartStore((s) => s.items);
   const totalItems = items.reduce((acc, item) => acc + item.quantity, 0);
-  const total = items.reduce(
+  const totalPrice = items.reduce(
     (acc, item) => acc + item.price * item.quantity,
     0,
   );
@@ -46,38 +40,123 @@ export default function Navbar() {
           >
             Order
           </Link>
-          <Drawer>
-            {items.length > 0 && (
-              <DrawerTrigger>Cart ({totalItems})</DrawerTrigger>
-            )}
-            <DrawerContent>
-              <DrawerHeader>
-                <DrawerTitle className="flex items-baseline gap-2">
-                  <span className="text-xl">
-                    Cart {items.length > 0 && <span>({totalItems})</span>}
-                  </span>
-                </DrawerTitle>
-              </DrawerHeader>
-              <div className="max-h-96 overflow-y-auto">
-                {items.map((pizza) => (
-                  <CartItem key={pizza.name} pizza={pizza} />
-                ))}
-              </div>
-              <DrawerFooter>
-                <hr className="my-4" />
-                <div className="flex justify-between">
-                  <div className="mb-4 flex w-full justify-between text-xl">
-                    <span>Total: </span>
-                    <span className="font-semibold">${total}</span>
-                  </div>
-                </div>
-                <Button>Order Now</Button>
-              </DrawerFooter>
-            </DrawerContent>
-          </Drawer>
+          {isDesktop ? (
+            <CartSheet
+              items={items}
+              totalItems={totalItems}
+              totalPrice={totalPrice}
+            />
+          ) : (
+            <CartDrawer
+              items={items}
+              totalItems={totalItems}
+              totalPrice={totalPrice}
+            />
+          )}
         </nav>
       </div>
     </header>
+  );
+}
+
+import {
+  Sheet,
+  SheetContent,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+export function CartSheet({
+  items,
+  totalItems,
+  totalPrice,
+}: {
+  items: CartItem[];
+  totalItems: number;
+  totalPrice: number;
+}) {
+  return (
+    <Sheet>
+      {items.length > 0 && <SheetTrigger>Cart ({totalItems})</SheetTrigger>}
+      <SheetContent className="overflow-auto">
+        <SheetHeader>
+          <SheetTitle className="flex items-baseline gap-2">
+            <span className="text-xl">
+              Cart {items.length > 0 && <span>({totalItems})</span>}
+            </span>
+          </SheetTitle>
+        </SheetHeader>
+        <div>
+          {items.map((pizza) => (
+            <CartItem key={pizza.name} pizza={pizza} />
+          ))}
+        </div>
+        <SheetFooter>
+          <div className="w-full">
+            <hr className="my-4" />
+            <div className="mb-4 flex w-full justify-between text-xl">
+              <span>Total: </span>
+              <span className="font-semibold">${totalPrice.toFixed(2)}</span>
+            </div>
+
+            <div>
+              <Button className="w-full">Order Now</Button>
+            </div>
+          </div>
+        </SheetFooter>
+      </SheetContent>
+    </Sheet>
+  );
+}
+
+import {
+  Drawer,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+
+function CartDrawer({
+  items,
+  totalItems,
+  totalPrice,
+}: {
+  items: CartItem[];
+  totalItems: number;
+  totalPrice: number;
+}) {
+  return (
+    <Drawer>
+      {items.length > 0 && <DrawerTrigger>Cart ({totalItems})</DrawerTrigger>}
+      <DrawerContent>
+        <DrawerHeader>
+          <DrawerTitle className="flex items-baseline gap-2">
+            <span className="text-xl">
+              Cart {items.length > 0 && <span>({totalItems})</span>}
+            </span>
+          </DrawerTitle>
+        </DrawerHeader>
+        <div className="max-h-96 overflow-y-auto">
+          {items.map((pizza) => (
+            <CartItem key={pizza.name} pizza={pizza} />
+          ))}
+        </div>
+        <DrawerFooter>
+          <hr className="my-4" />
+          <div className="flex justify-between">
+            <div className="mb-4 flex w-full justify-between text-xl">
+              <span>Total: </span>
+              <span className="font-semibold">${totalPrice.toFixed(2)}</span>
+            </div>
+          </div>
+          <Button>Order Now</Button>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
   );
 }
 
